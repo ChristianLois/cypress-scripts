5858c4c8aae84bb15971a030235e5dd0febb0f9c
christianolo <cvolo@up.edu.ph>
2022-08-01 16:19:45 +0700

    added a package for reporting tests.

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://52.76.214.142/'
+    baseUrl: 'https://52.76.214.142/',
+    reporter: 'mochawesome',
+    reporterOptions: {
+      overwrite: false,
+      html: false,
+      json: true
+    }
   },
 });
