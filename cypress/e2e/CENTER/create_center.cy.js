692f9bf81c790957474dd2cd5ee9ce9606ead2f5
Allen Tam <altam@up.edu.ph>
2022-08-03 16:47:10 +0800

    Create Center

--- cypress/e2e/CENTER/create_center.cy.js
+++ cypress/e2e/CENTER/create_center.cy.js
@@ -1,44 +1,65 @@
 // Author: Allen Tam
-
+import COMPONENT_HEADER from "../../resources/COMPONENTS/HEADER/component_header.json";
+import PAGE_HOME from "../../resources/PAGES/HOME/page_home.json";
+import PAGE_LOGIN from "../../resources/PAGES/AUTHENTICATION/page_login.json";
+import EXPECTED_LOGIN from "../../data/EXPECTED/AUTHENTICATION/expected_login.json";
+import INPUT_LOGIN from "../../data/INPUTS/AUTHENTICATION/input_login.json";
+import CENTER from "../../resources/PAGES/CENTER/page_center.json";
+import EXPECTED_CENTER from "../../data/CENTER/CREATE_CENTER/expected_create_center.json";
+import COMPONENT_CENTER from "../../resources/COMPONENTS/CENTER/component_center.json";
 import { centerName } from "../../common.cy";
 
 describe("create a center", () => {
   // Setup
   beforeEach(() => {
     cy.initPage();
-    cy.login("mifos", "password");
-    cy.url().should("include", "/home");
+    cy.login(INPUT_LOGIN.VALID.USERNAME, INPUT_LOGIN.VALID.PASSWORD);
+
+    cy.url().should("include", EXPECTED_LOGIN.VALID.PATH_URL);
+    cy.get(PAGE_HOME.TEXT_WELCOME).should(
+      "contain.text",
+      EXPECTED_LOGIN.VALID.TEXT_WELCOME
+    );
     // Teardown
   });
   afterEach(() => {
-    cy.get("#user-dropdown").trigger("mouseover");
-    cy.get("#logout").click();
-    cy.get("#uid").should("be.visible");
+    cy.get(COMPONENT_HEADER.DROPDOWN_MIFOS).trigger("mouseover");
+    cy.get(COMPONENT_HEADER.BUTTON_LOGOUT).click();
+    cy.get(PAGE_LOGIN.TEXTFIELD_USERNAME).should("be.visible");
   });
   it("create a center", { tags: ["@happy_path"] }, () => {
-    cy.get(".paddedleft strong").should("contain.text", "Welcome, mifos");
+    cy.get(EXPECTED_CENTER.VALID_CENTER_CREATION.HEADER_TEXT).should(
+      "contain.text",
+      "Welcome, mifos"
+    );
     cy.wait(1000);
-    cy.get(".left-nav .fa-map-marker").click();
-    cy.get("#name").type(centerName);
-    cy.get("#save").click();
+    cy.get(COMPONENT_CENTER.NAVBAR_CENTER_BUTTON).click();
+    cy.get(CENTER.CREATE_CENTER.TEXTFIELD_CENTER_NAME).type(centerName);
+    cy.get(CENTER.CREATE_CENTER.SUBMIT_BTN).click();
     //assertion
-    cy.url().should("include", "/#/viewcenter/");
+    cy.url().should("include", EXPECTED_CENTER.VALID_CENTER_CREATION.PATH_URL);
     cy.contains("Summary");
 
     //Delete Center
-    cy.get("span.btn-group.dropdown > .btn.btn-primary").click();
-    cy.get(".lli.ng-binding,[has-permission='DELETE_CENTER']").click();
-    cy.get(".modal-body > .btn-primary").click();
+    cy.get(CENTER.DELETE_CENTER.DROPDOWN_BTN).click();
+    cy.get(CENTER.DELETE_CENTER.DELETE_BTN).click();
+    cy.get(CENTER.DELETE_CENTER.CONFIRM_BTN).click();
   });
   it("create an invalid center", { tags: ["@negative"] }, () => {
-    cy.get(".paddedleft strong").should("contain.text", "Welcome, mifos");
+    cy.get(EXPECTED_CENTER.VALID_CENTER_CREATION.HEADER_TEXT).should(
+      "contain.text",
+      "Welcome, mifos"
+    );
     cy.wait(1000);
-    cy.get(".left-nav .fa-map-marker").click();
-    cy.get("#name").type(" ");
-    cy.get("#save").click();
+    cy.get(COMPONENT_CENTER.NAVBAR_CENTER_BUTTON).click();
+    cy.get(CENTER.CREATE_CENTER.TEXTFIELD_CENTER_NAME).type(" ");
+    cy.get(CENTER.CREATE_CENTER.SUBMIT_BTN).click();
 
     //assertion
-    cy.url().should("include", "/#/createcenter");
+    cy.url().should(
+      "include",
+      EXPECTED_CENTER.INVALID_CENTER_CREATION.PATH_URL
+    );
     cy.contains("Required Field");
   });
 });
