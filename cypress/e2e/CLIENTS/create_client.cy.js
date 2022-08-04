f2e1f5a828cbefdef487eee465b7749e817ba7b4
Vince Gelo <vdbuala@up.edu.ph>
2022-08-04 16:37:02 +0800

    Added series of data in testing creation of clients

--- cypress/e2e/CLIENTS/create_client.cy.js
+++ cypress/e2e/CLIENTS/create_client.cy.js
@@ -6,6 +6,7 @@ import CREATE_CLIENT from "../../resources/PAGES/CLIENT/page_create_client.json"
 import VIEW_CLIENT from "../../resources/PAGES/CLIENT/page_view_client.json";
 import EXPECTED_CREATE_CLIENT from "../../data/EXPECTED/CLIENT/expected_create_client.json"
 
+
 describe('Create Client', { tags : '@client' } , function () { 
   // Setup
   beforeEach(function () {
@@ -89,6 +90,41 @@ describe('Create Client', { tags : '@client' } , function () {
       cy.get(CREATE_CLIENT.FORM.SUBMIT_BTN).click()
       cy.get(CREATE_CLIENT.INVALID.ERRORS.PAST_ACTIVATION_DATE)
         .should('include.text', EXPECTED_CREATE_CLIENT.ACTIVATION_DATE_MESSAGE)
+    })
+  })
+})
+
+//series of data - static version
+describe('Create Client Series of Data Birthday', () => {
+
+  const fixtures = [
+    {date: new Date("10 June 2000"),testname:"Create Client Birthday Before Current Date", input_date: "10 June 2000"},
+    {date: new Date("04 August 2022"),testname:"Create Client Birthday On Current Date", input_date: "04 August 2022"},
+    {date: new Date("25 December 2022"),testname:"Create Client Birthday After Current Date", input_date: "25 December 2022"}
+  ]
+  
+  const currentDate = new Date();
+
+  fixtures.forEach(fixture => {
+    describe("Creating a Client with Birthday From " + fixture.input_date, () => {
+      before(function () {
+        cy.initPage()
+        cy.login(INPUT_LOGIN.VALID.USERNAME, INPUT_LOGIN.VALID.PASSWORD)
+        cy.navigateToCreateClient()  
+      })
+      it(fixture.testname, function(){
+        cy.optionalDetails(clientDetails.firstName, clientDetails.middleName, clientDetails.lastName, 
+          clientDetails.mobileNo, fixture.input_date)
+
+        if(fixture.date <= currentDate){
+          cy.get(VIEW_CLIENT.TITLE_NAME).should('include.text', clientDetails.fullName)
+          cy.url().should('include', EXPECTED_CREATE_CLIENT.URLS.VIEW_CLIENT)
+          cy.deleteClient(clientDetails.fullName)
+        }else{
+          cy.contains("validation.msg.client.dateOfBirth.is.greater.than.date - ")
+        }
+      })
+
     })
   })
 })
