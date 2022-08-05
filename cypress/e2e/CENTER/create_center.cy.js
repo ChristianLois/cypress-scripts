53560a627ec65fbf8ee79e65cb00bb321940993d
Allen Tam <altam@up.edu.ph>
2022-08-05 09:55:06 +0800

    Updated Create Center

--- cypress/e2e/CENTER/create_center.cy.js
+++ cypress/e2e/CENTER/create_center.cy.js
@@ -9,7 +9,7 @@ import EXPECTED_CENTER from "../../data/EXPECTED/CREATE_CENTER/expected_create_c
 import COMPONENT_CENTER from "../../resources/COMPONENTS/CENTER/component_center.json";
 import { generateCenterDetails } from "../../common.cy";
 
-describe("create a center", () => {
+describe("create a center", { tags: "@center" }, () => {
   const centerName = generateCenterDetails();
   // Setup
   beforeEach(() => {
@@ -21,21 +21,16 @@ describe("create a center", () => {
       "contain.text",
       EXPECTED_LOGIN.VALID.TEXT_WELCOME
     );
-  // Teardown
+    // Teardown
   });
   afterEach(() => {
-    cy.get(COMPONENT_HEADER.DROPDOWN_MIFOS).trigger("mouseover");
-    cy.get(COMPONENT_HEADER.BUTTON_LOGOUT).click();
-    cy.get(PAGE_LOGIN.TEXTFIELD_USERNAME).should("be.visible");
+    cy.get(CENTER.DELETE_CENTER.DROPDOWN_BTN).click();
+    cy.get(CENTER.DELETE_CENTER.DELETE_BTN).click();
+    cy.get(CENTER.DELETE_CENTER.CONFIRM_BTN).click();
   });
   it(
     "create a center",
-    {
-      tags: [
-        "smoke_test",
-        "create_a_center_positive_test",
-      ],
-    },
+    { tags: ["smoke_test", "create_a_center_positive_test"] },
     () => {
       cy.get(EXPECTED_CENTER.VALID_CENTER_CREATION.HEADER_TEXT).should(
         "contain.text",
@@ -43,6 +38,7 @@ describe("create a center", () => {
       );
       cy.wait(1000);
       cy.get(COMPONENT_CENTER.NAVBAR_CENTER_BUTTON).click();
+      //cy.navigateToCenter();
       cy.get(CENTER.CREATE_CENTER.TEXTFIELD_CENTER_NAME).type(centerName);
       cy.get(CENTER.CREATE_CENTER.SUBMIT_BTN).click();
       //assertion
@@ -51,15 +47,29 @@ describe("create a center", () => {
         EXPECTED_CENTER.VALID_CENTER_CREATION.PATH_URL
       );
       cy.contains("Summary");
-
-      //Delete Center
-      cy.get(CENTER.DELETE_CENTER.DROPDOWN_BTN).click();
-      cy.get(CENTER.DELETE_CENTER.DELETE_BTN).click();
-      cy.get(CENTER.DELETE_CENTER.CONFIRM_BTN).click();
     }
   );
+});
+describe("create an invalid center", { tags: "@center" }, function () {
+  // Setup
+  beforeEach(() => {
+    cy.initPage();
+    cy.login(INPUT_LOGIN.VALID.USERNAME, INPUT_LOGIN.VALID.PASSWORD);
+
+    cy.url().should("include", EXPECTED_LOGIN.VALID.PATH_URL);
+    cy.get(PAGE_HOME.TEXT_WELCOME).should(
+      "contain.text",
+      EXPECTED_LOGIN.VALID.TEXT_WELCOME
+    );
+    // Teardown
+  });
+  afterEach(() => {
+    cy.get(COMPONENT_HEADER.DROPDOWN_MIFOS).trigger("mouseover");
+    cy.get(COMPONENT_HEADER.BUTTON_LOGOUT).click();
+    cy.get(PAGE_LOGIN.TEXTFIELD_USERNAME).should("be.visible");
+  });
   it(
-    "create an invalid center",
+    "create a center with blank name",
     {
       tags: [
         "smoke_test",
